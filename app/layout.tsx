fd6fdb3e4950981f399c8b9aac7a355019d983fe
anantkumar5600-hub <anantkumar5600@gmail.com>
2026-09-21 22:26:43 +0530

    apna tame aayega

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque, IBM_Plex_Sans, IBM_Plex_Mono } from "next/font/google";
-// @ts-expect-error Next.js processes this stylesheet import at build time.
 import "./globals.css";
 import CustomCursor from "@/components/CustomCursor";
-// HELLO
+
 const display = Bricolage_Grotesque({
   subsets: ["latin"],
   variable: "--font-display",
